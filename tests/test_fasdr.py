15be5d229b96a94c224e30363bcd8e52f7cd94cb
David Marx <david.marx84@gmail.com>
2023-04-19 13:34:28 -0700

    cleanup tests, version bump

--- tests/test_fasdr.py
+++ tests/test_fasdr.py
@@ -1,73 +1,4 @@
 import pytest
-import numpy as np
-from pathlib import Path
-from tempfile import TemporaryDirectory
-import scipy
-
-from fasdr import (
-    create_kdtree,
-    search_index,
-    create_nlp_pipeline,
-    Document,
-    DocumentIndex,
-)
-
-D_EMBED = 384
-
-# Dummy sentence embeddings
-sentences = ["This is a test.", "Another test sentence.", "The third test sentence."]
-embeddings = np.random.rand(3, D_EMBED)
-
-# Temporary directory for storing test files
-temp_dir = TemporaryDirectory()
-doc_path = Path(temp_dir.name) / "test.txt"
-doc_path.write_text("\n".join(sentences))
-
-def test_create_kdtree_index():
-    index = create_kdtree(embeddings)
-    assert isinstance(index, scipy.spatial.KDTree)
-
-def test_search_index():
-    index = create_kdtree(embeddings)
-    query = np.random.rand(1, D_EMBED)
-    distances, indices = search_index(index, query, k=2)
-    #assert distances.shape == (1, 2)
-    #assert indices.shape == (1, 2)
-    assert distances.shape == (2,)
-    assert indices.shape == (2,)
-
-def test_create_nlp_pipeline():
-    nlp = create_nlp_pipeline()
-    assert "sentencizer" in nlp.pipe_names
-    assert "sentence_bert" in nlp.pipe_names
-
-def test_document():
-    with TemporaryDirectory() as temp_dir:
-        doc_path = Path(temp_dir) / "test.txt"
-        doc_path.write_text("\n".join(sentences))
-        doc = Document(doc_path)
-        assert len(doc.sentences) == 3
-        query = np.random.rand(1, D_EMBED)
-        results = doc.search_sentences(query, k=2)
-        assert len(results) == 2
-
-def test_document_index():
-    with TemporaryDirectory() as temp_dir:
-        doc_path = Path(temp_dir) / "test.txt"
-        doc_path.write_text("\n".join(sentences))
-        index = DocumentIndex(temp_dir)
-        assert len(index.documents) == 1
-        query = "test query"
-        doc_results = index.search_documents(query, k=1)
-        assert len(doc_results) == 1
-        sentence_results = index.search_sentences(query, k=1)
-        assert len(sentence_results) == 1
-        assert len(sentence_results[0]) == 3
-        #assert len(sentence_results[0][-1]) == 1
-
-##########################################
-
-import pytest
 import tempfile
 import shutil
 import os
@@ -77,6 +8,7 @@ from typing import List
 import numpy as np
 import spacy
 from scipy.spatial import KDTree
+import scipy
 
 from fasdr import (
     build_targeted_index,
@@ -89,11 +21,29 @@ from fasdr import (
     search_index,
 )
 
+D_EMBED = 384
+
+# Dummy sentence embeddings
+sentences = ["This is a test.", "Another test sentence.", "The third test sentence."]
+embeddings = np.random.rand(3, D_EMBED)
+
+# Temporary directory for storing test files
+temp_dir = tempfile.TemporaryDirectory()
+doc_path = Path(temp_dir.name) / "test.txt"
+doc_path.write_text("\n".join(sentences))
+
+
+def create_sample_files(root_dir: Path, extensions: List[str]):
+    for ext in extensions:
+        subdir = root_dir / ext.strip(".")
+        subdir.mkdir(parents=True, exist_ok=True)
+        (subdir / f"sample{ext}").        write_text(f"This is a sample {ext} file.")
+
+
 def test_create_kdtree():
     data = np.array([[1, 2], [3, 4], [5, 6]])
     tree = create_kdtree(data)
     assert isinstance(tree, KDTree)
-
 
 def test_update_kdtree():
     data = np.array([[1, 2], [3, 4], [5, 6]])
@@ -103,6 +53,17 @@ def test_update_kdtree():
     assert isinstance(updated_tree, KDTree)
     assert updated_tree.data.shape == (4, 2)
 
+def test_search_index():
+    data = np.array([[1, 2], [3, 4], [5, 6]])
+    tree = create_kdtree(data)
+    query = np.array([[1, 2]])
+    k=1
+    distances, indices = search_index(tree, query, k=k)
+    assert distances.shape == (k,)
+    assert indices.shape == (k,)
+
+    assert np.array_equal(distances, np.array([0]))
+    assert np.array_equal(indices, np.array([0]))
 
 def test_create_nlp_pipeline():
     nlp = create_nlp_pipeline()
@@ -110,15 +71,28 @@ def test_create_nlp_pipeline():
     assert nlp.has_pipe("sentencizer")
     assert nlp.has_pipe("sentence_bert")
 
+def test_document():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        doc_path = Path(temp_dir) / "test.txt"
+        doc_path.write_text("\n".join(sentences))
+        doc = Document(doc_path)
+        assert len(doc.sentences) == 3
+        query = np.random.rand(1, D_EMBED)
+        results = doc.search_sentences(query, k=2)
+        assert len(results) == 2
 
-def test_search_index():
-    data = np.array([[1, 2], [3, 4], [5, 6]])
-    tree = create_kdtree(data)
-    query = np.array([[1, 2]])
-    distances, indices = search_index(tree, query)
-    assert np.array_equal(distances, np.array([0]))
-    assert np.array_equal(indices, np.array([0]))
-
+def test_document_index():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        doc_path = Path(temp_dir) / "test.txt"
+        doc_path.write_text("\n".join(sentences))
+        index = DocumentIndex(temp_dir)
+        assert len(index.documents) == 1
+        query = "test query"
+        doc_results = index.search_documents(query, k=1)
+        assert len(doc_results) == 1
+        sentence_results = index.search_sentences(query, k=1)
+        assert len(sentence_results) == 1
+        assert len(sentence_results[0]) == 3
 
 def test_document_empty_file():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -127,7 +101,6 @@ def test_document_empty_file():
 
         with pytest.raises(EmptyDocument):
             Document(temp_file)
-
 
 def test_document_simple_file():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -138,7 +111,6 @@ def test_document_simple_file():
         assert len(doc.sentences) == 1
         assert doc.sentences[0] == "This is a test."
         assert doc.sentence_embeddings is not None
-
 
 def test_document_save_and_load_embeddings():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -154,7 +126,6 @@ def test_document_save_and_load_embeddings():
         assert loaded_doc.sentences == doc.sentences
         assert np.array_equal(loaded_doc.sentence_embeddings, doc.sentence_embeddings)
 
-
 def test_document_search_sentences():
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_file = Path(temp_dir) / "simple.txt"
@@ -167,12 +138,6 @@ def test_document_search_sentences():
         results = doc.search_sentences(query_embedding)
         assert len(results) == 1
         assert results[0][1] == "This is a test."
-
-def create_sample_files(root_dir: Path, extensions: List[str]):
-    for ext in extensions:
-        subdir = root_dir / ext.strip(".")
-        subdir.mkdir(parents=True, exist_ok=True)
-        (subdir / f"sample{ext}").        write_text(f"This is a sample {ext} file.")
 
 def test_document_index_empty_directory():
     with tempfile.TemporaryDirectory() as temp_dir:
