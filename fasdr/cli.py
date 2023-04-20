aa33943e748a3282b9bb14576856d983b92992b3
David Marx <david.marx84@gmail.com>
2023-04-20 11:41:31 -0700

    Update cli.py - debugging

--- fasdr/cli.py
+++ fasdr/cli.py
@@ -1,17 +1,30 @@
 import argparse
 import os
 import sys
+import logging
+import rich
+from rich.console import Console
+from rich.logging import RichHandler
 
 from fasdr import DocumentIndex
-from rich.console import Console
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+logger = logging.getLogger("rich")
+logger.info("Hello, World!")
 
 def main():
     console = Console()
     parser = argparse.ArgumentParser(description="Fasdr: Fast Approximate String Distance Retrieval")
     parser.add_argument("path", help="Path to the index directory")
     args = parser.parse_args()
-
+    logger.debug(args)
     index_path = args.path
+    logger.debug(index_path)
+    logger.debug(sys.argv)
     if not os.path.exists(index_path):
         console.print(f"Error: Path '[red]{index_path}[/red]' does not exist.", style="bold red")
         sys.exit(1)
